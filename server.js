bc01ab6abc21a7e0628980a1b5149772c30d53f3
Benny <johansson.benny@gmail.com>
2020-06-15 21:18:54 +0200

    testg

--- server.js
+++ server.js
@@ -156,6 +156,7 @@ app.get('/ekosim/timetable/update/:lastTimestamp', (req, res, next) => {
 
 app.put('/ekosim/put/:parameterID', function (req, res) {
 
+    
     var ParameterID = req.params.parameterID;
     var value = req.body.VALUE;
     //console.log(value);
