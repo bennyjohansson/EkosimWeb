65bb8a70968532bf4a19d70961416a3c0b63a6c3
Benny <johansson.benny@gmail.com>
2020-11-22 09:56:49 +0100

    bankview and new parameter API

--- DBConnection.js
+++ DBConnection.js
@@ -75,6 +75,44 @@ getWorldTable = function(table) { //database, table
     });
 }
 
+getTable = function(myDatabase, table) { //database, table
+    console.log(myDatabase);
+    return new Promise((resolve, reject) => {
+        //let db = new sqlite3.Database('/home/ec2-user/ekosimProject/myDB/Bennyland.db', sqlite3.OPEN_READONLY, (err) => {
+            //'./myDB/Bennyland.db'
+            
+        let db = new sqlite3.Database(myDatabase, sqlite3.OPEN_READONLY, (err) => {
+            if (err) {
+                console.error(err.message);
+            }
+            console.log('Connected to the ekosim database.');
+        });
+        const queries = [];
+        //console.log(`SELECT rowid as key, * FROM ${table} WHERE TIME > ${lastTime}`);
+        db.each(`SELECT rowid as key, * FROM ${table}`, (err, row) => {
+        //db.each(`SELECT rowid as key, * FROM MONEY_DATA`, (err, row) => {
+            if (err) {
+                reject(err); // optional: you might choose to swallow errors.
+            } else {
+                queries.push(row); // accumulate the data
+            }
+        }, (err, n) => {
+            if (err) {
+                reject(err); // optional: again, you might choose to swallow this error.
+            } else {
+                resolve(queries); // resolve the promise
+            }
+        });
+        // close the database connection
+        db.close((err) => {
+            if (err) {
+                console.error(err.message);
+            }
+            console.log('Close the database connection.');
+        });
+    });
+}
+
 
 var insertFunction = function (myDatabase, parameter, value) {
     //'./myDB/Bennyland.db'
--- server.js
+++ server.js
@@ -101,47 +101,31 @@ app.get('/ekosim/read/:myCountry', (req, res, next) => {
 
 });
 
-app.get('/ekosim/readallparameters/:myCountry', (req, res, next) => {
+app.get('/ekosim/getAllParameters/:myCountry', (req, res, next) => {
 
     var myPath = './myDB/';
     var myCountry = req.params.myCountry; // //
     var myDatabase = myPath.concat(myCountry);
     myDatabase = myDatabase.concat('.db');
 
-    //var params = [req.query.parameterID];
+    //myDatabase = './myDB/Bennyland.db';
 
-    //let db = new sqlite3.Database('/home/ec2-user/ekosimProject/myDB/ekosimDB.db', sqlite3.OPEN_READONLY, (err) => {
-    let db = new sqlite3.Database(myDatabase, sqlite3.OPEN_READONLY, (err) => {
 
-        //./app/app.js
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('Connected to the ekosim database.');
-    });
+    console.log(myDatabase);
+    myTable = getTable(myDatabase, 'PARAMETERS');
+     
 
-    var sql = "select * from PARAMETERS"// InterestRateMethod TargetInterestRate
-    //params = [];
-    //console.log(sql);
-    //console.log(params);
-    db.get(sql, (err, row) => {
-        if (err) {
-            res.status(400).json({ "error": err.message });
-            return;
-        }
-        res.json({
+    var mytableJSON = myTable.then((result) => {
+        //console.log(result[31]) // "Some User token"
+        //return result[31];
+        return res.json({
             "message": "success",
-            "data": row
+            "data": result
         })
     });
 
-    //Closing the database
-    db.close((err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('Close the database connection.');
-    });
+
+    //console.log(mytableJSON)
 
 });
 
